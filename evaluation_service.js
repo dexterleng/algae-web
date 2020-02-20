184b1979ec68e82075bed270f14fa942951e64a1
Dexter Leng <dexterlengcs@gmail.com>
2020-02-20 15:18:14 +0800

    Hello

---
+++ evaluation_service.js
@@ -0,0 +1,111 @@
+const Docker = require('dockerode');
+const tar = require("tar-fs");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+
+const imageName = "dexterleng/algae-dev:latest";
+
+class EvaluationService {
+    static createConnection() {
+        return new Docker({ socketPath: '/var/run/docker.sock' });
+    }
+
+    static pullImage(conn) {
+        return new Promise(async (resolve, reject) => {
+            if (await EvaluationService.imageExists(conn)) {
+                resolve(conn.getImage(imageName));
+                return;
+            };
+
+            conn.pull(imageName, (pullError, stream) => {
+                if (pullError) {
+                    reject(pullError);
+                    return;
+                }
+
+                if (!stream) {
+                    throw new Error(`Image doesn't exists`);
+                }
+
+                conn.modem.followProgress(stream, (error, output) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+    
+                    resolve(conn.getImage(imageName));
+                });
+            });
+        });
+    }
+
+    static async imageExists(conn) {
+        const images = await conn.listImages({ filters: { reference: [imageName] } });
+        return images.length > 0;
+    }
+
+    static createContainer(conn) {
+        return conn.createContainer({
+            Image: imageName,
+            AttachStdin: false,
+            AttachStdout: true,
+            AttachStderr: true,
+            Tty: true,
+            Cmd: ['/bin/bash', '-c', 'tail -f /var/log/dmesg'],
+            OpenStdin: false,
+            StdinOnce: false
+        });
+    }
+
+    // Reference:
+    // https://github.com/zojeda/docker-workspace/blob/f1672d50631f79d06a4c64f2a6f07f691352755d/src/FSCopyProvisioner.ts
+    static async moveToContainer(container, sourcePath, destPath) {
+        return new Promise((resolve, reject) => {
+            const tmpDir = path.join(os.tmpdir(), "algae-tmp");
+            if(!fs.existsSync(tmpDir)) {
+                fs.mkdirSync(tmpDir);
+            }
+    
+            const tarPath = path.join(tmpDir, "projects.tar");
+            tar.pack(sourcePath).pipe(fs.createWriteStream(tarPath))
+                .on("finish", async () => {
+                    await container.putArchive(tarPath,{ path: destPath });
+                    resolve();
+                }); 
+        });
+    }
+
+    static async extractFromContainer(container, sourcePath, destPath) {
+        return new Promise(async (resolve, reject) => {
+            const readableStream = await container.getArchive({
+                path: sourcePath
+            });
+    
+            const tmpDir = path.join(os.tmpdir(), "algae-tmp");
+            if(!fs.existsSync(tmpDir)) {
+                fs.mkdirSync(tmpDir);
+            }
+    
+            const tarPath = path.join(tmpDir, "results.tar");
+            const writableStream = fs.createWriteStream(tarPath);
+            readableStream.pipe(writableStream);
+            readableStream.on("end", () => {
+                const readableTar = fs.createReadStream(tarPath);
+                readableTar.pipe(tar.extract(destPath));
+                readableTar.on("end", () => {
+                    resolve();
+                });
+            });
+        });
+    }
+
+    static async startContainerAndWait(container) {
+        await container.start()
+        await container.wait({
+            condition: "not-running"
+        });
+    }
+}
+
+module.exports = EvaluationService;
--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
-const ejs = require('ejs');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra')
+const Queue = require('bull');
+
+const EvaluationService = require("./evaluation_service");
+const evalServiceConn = EvaluationService.createConnection();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const REDIS_URL = process.env.REDIS_URL;
+
+const compareJobQueue = new Queue("comparison", REDIS_URL);
+
+compareJobQueue.process(async (job, done) => {
+	try {
+		fs.emptyDirSync("./static/compare_results/");
+		fs.rmdirSync("./static/compare_results/");
+
+		console.log("Pulling algae image...");
+		await EvaluationService.pullImage(evalServiceConn);
+		console.log("Image pulled.");
+		const container = await EvaluationService.createContainer(evalServiceConn);
+		console.log("Container created.");
+		await EvaluationService.moveToContainer(container, "./static/projects/", "/usr/src/app/projects/");
+		console.log("Moved projects to container.");
+		await EvaluationService.startContainerAndWait(container);
+		console.log("Container has started");
+		await EvaluationService.extractFromContainer(container, "/usr/src/app/compare_results/", "./static/");
+		console.log("Comparison results have been extracted.");
+		done(null, null);
+	} catch (e) {
+		done(e);
+	}
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,18 +41,17 @@ app.set('view engine', 'ejs');
 // set path for static assets
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
-
 app.get('/', (req, res) => {
 	res.render('index', { });
 });
 
 app.get('/summary', (req, res) => {
-	const file_compare_summary = JSON.parse(fs.readFileSync("./static/file_compare_results/summary.json"));
+	const file_compare_summary = JSON.parse(fs.readFileSync("./static/compare_results/summary.json"));
 	res.render('summary', { file_compare_summary });
 });
 
 app.get('/file_compare/:file_compare_filename', (req, res) => {
-	const file_compare_path = `/static/file_compare_results/${req.params.file_compare_filename}`;
+	const file_compare_path = `/static/compare_results/${req.params.file_compare_filename}`;
 	const file_compare_file = JSON.parse(fs.readFileSync("." + file_compare_path, "utf8"));
 	const file_a_name = file_compare_file.project_a_file.file_dir;
 	const file_b_name = file_compare_file.project_b_file.file_dir;
@@ -32,6 +59,26 @@ app.get('/file_compare/:file_compare_filename', (req, res) => {
 	const file_b_path = path.join("/static/projects/", file_b_name);
 	
 	res.render('file_compare', { file_a_path, file_b_path, file_a_name, file_b_name, file_compare_path });
+});
+
+app.post('/start_compare', async (req, res) => {
+	if (!fs.existsSync("./static/projects/")) {
+		res.status(400).send();
+		return;
+	};
+
+	const job = await compareJobQueue.add({});
+	res.status(201).send(job);
+});
+
+app.get("/compare_progress/:jobID", async (req, res) => {
+	const { jobID } = req.params;
+	if (jobID === null || jobID === undefined) {
+		res.status(400).send();
+	};
+
+	const job = await compareJobQueue.getJob(jobID);
+	res.json(job);
 });
 
 app.listen(PORT, () => {
